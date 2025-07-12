e51beca8a7219cc54032f5faf396a37c3716c420
parsalemi <parsalemi@outlook.com>
2025-07-12 20:39:39 +0330

    new skills and projets

---
+++ chunk-BDGLC6XZ.js
@@ -0,0 +1,1 @@
+var e={production:!0,staticFileUrl:"./assets",ecommerce_api_url:"https://ex-portfolio-pg.onrender.com/api",recaptcha:{sitekey:"6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc"}};export{e as a};
